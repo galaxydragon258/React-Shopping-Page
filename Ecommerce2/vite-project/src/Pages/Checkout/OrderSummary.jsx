78da2835d7f8bd7f81325c876329199908320690
Mark <mjustinee258@gmail.com>
2026-04-02 10:22:51 +0800

    create delete cart item feature

--- Ecommerce2/vite-project/src/Pages/Checkout/OrderSummary.jsx
+++ Ecommerce2/vite-project/src/Pages/Checkout/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 import { DeliveryOptions } from "./DeliveryOptions"
 import { formatMoney } from "../utils/money"
+import axios from 'axios'
 export function OrderSummary({ cart, deliveryOptions,getCartItems,getPaymentSummary }) {
     return (
 
@@ -12,6 +13,11 @@ export function OrderSummary({ cart, deliveryOptions,getCartItems,getPaymentSumm
             )
             
                 const formattedDelivery = selectedDeliveryDate ?.estimatedDeliveryTimeMs?dayjs(selectedDeliveryDate.estimatedDeliveryTimeMs).format('dddd, MMMM D'): "No Delivery Date";
+
+                const deleteCartItem = async()=>{
+                    await axios.delete(`/api/cart-items/${cartItems.productId}`);
+                    await getCartItems();
+                }
 
                 return (
                     <div
@@ -41,7 +47,9 @@ export function OrderSummary({ cart, deliveryOptions,getCartItems,getPaymentSumm
                                     <span className="update-quantity-link link-primary">
                                         Update
                                     </span>
-                                    <span className="delete-quantity-link link-primary">
+                                    <span className="delete-quantity-link link-primary"
+                                        onClick={deleteCartItem}
+                                    >
                                         Delete
                                     </span>
                                 </div>
